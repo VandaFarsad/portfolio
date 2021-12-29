31dbb09aef9b6335f37bac7209348828445a26a4
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-29 15:05:57 +0100

    add test for split_paragraphs

--- portfolio/tests/test_views.py
+++ portfolio/tests/test_views.py
@@ -5,3 +5,8 @@ def test_split_into_blocks():
     view = BaseView()
     assert view._split_into_blocks([1, 1, 2, 2, 3, 3], 2) == [[1, 1], [2, 2], [3, 3]]
     assert view._split_into_blocks([1, 1, 2, 2, 3, 3]) == [[1, 1, 2], [2, 3, 3]]
+
+def test_split_paragraphs():
+    view = BaseView()
+    assert view._split_paragraphs("This makes\pno sense!") == ["This makes", "no sense!"]
+    assert view._split_paragraphs("This makes no sense!") == ["This makes no sense!"]
